2de68adc302013a1d37f28ec2869e89d5cab553a
Kasper Welbers <kasperwelbers@gmail.com>
2022-01-20 09:54:25 +0100

    working version

--- src/App.js
+++ src/App.js
@@ -1,19 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 // Main pages. Use below in items to include in header menu
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import { AnnotatorAPIClient } from "ccs-annotator-client";
+import Annotator, { Backend, JobServerAPI } from "ccs-annotator-client";
+import { Grid, Header, Icon } from "semantic-ui-react";
+
+import "style.css";
+
+const PORT = 8000;
+const TRY_EVERY = 1000;
 
 const App = () => {
-  return (
-    <Router>
-      <div style={{ height: "100vh", width: "100vw" }}>
-        <Routes>
-          <Route path="/" exact element={<AnnotatorAPIClient fixedRPort={8000} />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+  // A simple client that just looks for a backend on the host
+  // Assumes only 1 job (with job_id = 0 or just ignored in backend)
+  const [jobServer, setJobServer] = useState(null);
+
+  useEffect(() => {
+    if (jobServer) return null;
+
+    const interval = setInterval(() => {
+      login(setJobServer);
+    }, TRY_EVERY);
+
+    return () => clearInterval(interval);
+  }, [jobServer, setJobServer]);
+
+  if (!jobServer) {
+    return (
+      <Grid inverted textAlign="center" style={{ height: "100vh" }} verticalAlign="middle">
+        <Grid.Column style={{ maxWidth: "500px" }}>
+          <Icon
+            name="sync"
+            size="huge"
+            style={{
+              animation: "rotation 5s infinite linear",
+            }}
+          />
+          <Header>{`Connecting to port ${PORT}. If this takes forever, please make sure the server is actually running`}</Header>
+        </Grid.Column>
+      </Grid>
+    );
+  }
+
+  return <Annotator jobServer={jobServer} />;
+};
+
+const login = async (setJobServer) => {
+  try {
+    const backend = new Backend("http://localhost:" + PORT, null);
+    await backend.init();
+    const js = new JobServerAPI(backend, 0, setJobServer);
+    await js.init();
+    setJobServer(js);
+  } catch (e) {}
 };
 
 export default App;
