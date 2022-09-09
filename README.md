4b081b00527b5306b588319d3abd548ce0bd055d
Kasper Welbers <kasperwelbers@gmail.com>
2022-09-09 03:16:28 +0200

    works again

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# ccs-annotator-client
+# Annotinder R client
 
-This repo basically just imports ccs-annotator-client, but tweaks it and builds it so that it can be used in the ccsAnnotator R package.
+This repo basically just imports the annotinder-client, but tweaks it and builds it so that it can be used in the AnnoTinder R package.
 
 ```bash
-git clone https://github.com/ccs-amsterdam/ccs-annotator-client-r
-cd CCS_annotator
+git clone https://github.com/ccs-amsterdam/annotinder-r-client
+cd annotinder-r-client
 npm install
 ```
 
@@ -14,4 +14,4 @@ Build and bundle
 npm run build
 ```
 
-Then copy the build folder into ccsAnnotator/inst/ccs-annotator-client/
+Then copy the build folder into annotinder/inst/annotinder-client/
--- src/App.js
+++ src/App.js
@@ -1,34 +1,12 @@
-import React, { useState, useEffect } from "react";
-import { AnnotatorRClient } from "ccs-annotator-client";
-import "style.css";
+import React from "react";
+import { AnnotatorRClient, ResponsiveContainer } from "annotinder-client";
+import "./style.css";
 
 const App = () => {
-  const [size, setSize] = useState({
-    height: window.innerHeight,
-    width: document.documentElement.clientWidth,
-  });
-
-  useEffect(() => {
-    // use window.innerHeight for height, because vh on mobile is weird (can include the address bar)
-    // use document.documentElement.clientwidth for width, to exclude the scrollbar
-    const onResize = () => {
-      setSize({
-        height: window.innerHeight,
-        width: document.documentElement.clientWidth,
-      });
-    };
-    window.addEventListener("resize", onResize);
-    window.screen.orientation.addEventListener("change", onResize);
-    return () => {
-      window.removeEventListener("resize", onResize);
-      window.screen.orientation.removeEventListener("change", onResize);
-    };
-  });
-
   return (
-    <div style={{ height: `${size.height}px`, width: `${size.width}px` }}>
+    <ResponsiveContainer>
       <AnnotatorRClient />
-    </div>
+    </ResponsiveContainer>
   );
 };
 
